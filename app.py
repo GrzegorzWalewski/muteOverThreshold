bb859e90039484642a010daa1c59a65e5e5c74c6
Grzojda <grzojda@gmail.com>
2024-03-11 20:58:59 +0100

    gui 1.0

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import sounddevice as sd
 import numpy as np
 import tkinter as tk
+from tkinter import ttk
+import threading
 from pynput.keyboard import Key, Controller
 
 muted = False
-threshold = 0.14
 
 def change_mute():
     global muted
     muted = not muted
     keyboard = Controller()
-    # Simulate pressing the Alt key and then the = key
-    with keyboard.pressed(Key.alt):
-        keyboard.press('=')
-        keyboard.release('=')
-
+    keyboard.press(Key.pause)
+    keyboard.release(Key.pause)
+    
 def list_audio_devices():
     devices = sd.query_devices()
     return [device['name'] for device in devices]
@@ -22,73 +21,120 @@ def list_audio_devices():
 def create_gui():
     window = tk.Tk()
     window.tk.call('tk', 'scaling', 3)
-    window.title("Mute Over Threshold")
+    window.title("MuteOverThreshold")
+    window.iconbitmap('icon.ico')
+    window.geometry("500x800")
+    window.configure(bg="#1f1f1f")
+    window.resizable(False, False)
+
+    icon_path = "icon_mini.png"  # Replace with the actual path to your icon
+    icon = tk.PhotoImage(file=icon_path)
+    icon_label = tk.Label(window, image=icon, bg="#1f1f1f")
+    icon_label.pack()
+
+    threshold_label = tk.Label(window, text="RMS Threshold", bg="#1f1f1f", fg="#44e3f8", pady=10)
+    threshold_label.pack()
+    threshold_input = tk.Entry(window, bg="#1f1f1f", fg="#44e3f8")
+    threshold_input.config(bg="#1f1f1f")
+    threshold_input.insert(0, "0.07")
+    threshold_input.pack()
 
     device_combo = tk.StringVar(window)
     device_combo.set(list_audio_devices()[2])  # Default selected device
 
-    label_device = tk.Label(window, text="Select Audio Device:")
+    label_device = tk.Label(window, text="Select Audio Device:", bg="#1f1f1f", fg="#44e3f8", pady=10)
     label_device.pack()
 
     combo_device = tk.OptionMenu(window, device_combo, *list_audio_devices())
+    combo_device.config(bg="#1f1f1f", fg="#44e3f8", highlightthickness=0)
     combo_device.pack()
 
-    label_rms = tk.Label(window, text="Current RMS Value:")
+    label_rms = tk.Label(window, text="Current RMS Value:", bg="#1f1f1f", fg="#44e3f8", pady=10)
     label_rms.pack()
 
-    rms_value = tk.Label(window, text="")
+    rms_value = tk.Label(window, text="", bg="#1f1f1f", fg="#44e3f8", pady=10)
     rms_value.pack()
 
-    label_mute = tk.Label(window, text="Microphone Status:")
+    label_mute = tk.Label(window, text="Microphone Status:", bg="#1f1f1f", fg="#44e3f8", pady=10)
     label_mute.pack()
-    canvas = tk.Canvas(window, width=100, height=100)
+    canvas = tk.Canvas(window, width=100, height=100, bg="#1f1f1f", highlightthickness=0)
     canvas.pack()
 
     def start_monitoring():
+        canvas.create_oval(25, 25, 75, 75, fill="green")
         monitoring.set(True)
+        threshold_input.configure(state='readonly')
         selected_device = device_combo.get()
         def monitor():
             nonlocal selected_device
-            if monitoring.get():
-                current_rms = get_current_rms(device_name=selected_device)
-                rms_value.config(text=current_rms)
-                canvas.create_oval(25, 25, 75, 75, fill="red") if current_rms > threshold else canvas.create_oval(25, 25, 75, 75, fill="green")
-                window.after(1, monitor)  # Run again after 1 millisecond
-        monitor()  # Start monitoring
+            print(f"Checking {selected_device} with threshold {threshold_input.get()}")
+            
+            rms_values = rms_generator(device_name=selected_device)
+            for rms_val in rms_values:
+                if not monitoring.get():
+                    break
+                rms_value.config(text=round(rms_val, 2))
+
+                if rms_val > float(threshold_input.get()):
+                    if not muted:
+                        print(f"Too loud! Muting {rms_val:.2f}")
+                        canvas.delete("all")
+                        canvas.create_oval(25, 25, 75, 75, fill="red")
+                        change_mute()
+                else:
+                    if muted:
+                        print(f"Back to normal. Unmuting {rms_val:.2f}")
+                        canvas.delete("all")
+                        canvas.create_oval(25, 25, 75, 75, fill="green")
+                        change_mute()
+        threading.Thread(target=monitor).start()
 
     def stop_monitoring():
         monitoring.set(False)
+        threshold_input.configure(state='normal')
+        rms_value.config(text="-")
+        if muted:
+            change_mute()
+            canvas.create_oval(25, 25, 75, 75, fill="green")
 
-    start_button = tk.Button(window, text="Start Monitoring", command=start_monitoring)
-    start_button.pack()
+    def stop_program():
+        stop_monitoring()
+        window.destroy()
+        exit()
 
-    stop_button = tk.Button(window, text="Stop Monitoring", command=stop_monitoring)
-    stop_button.pack()
+    style = ttk.Style()
+    style.configure('Custom.TFrame', background="#1f1f1f")
 
-    exit_button = tk.Button(window, text="Exit", command=window.destroy)
-    exit_button.pack()
+    center_frame = ttk.Frame(window, style='Custom.TFrame')
+    center_frame.pack(pady=10)
+
+    start_button = tk.Button(center_frame, text="START", command=start_monitoring, bg="#1f1f1f", fg="green", padx=50)
+    start_button.grid(row=0, column=0, padx=5, pady=5)
+
+    stop_button = tk.Button(center_frame, text="STOP", command=stop_monitoring, bg="#1f1f1f", fg="red", padx=50)
+    stop_button.grid(row=0, column=1, padx=5, pady=5)
+
+    exit_button = tk.Button(window, text="EXIT", command=stop_program, bg="#1f1f1f", fg="#44e3f8", pady=10, padx=50)
+    exit_button.pack(side="top", pady=10)
 
     monitoring = tk.BooleanVar()
     monitoring.set(True)
 
+    rms_value.config(text="-")
+    canvas.create_oval(25, 25, 75, 75, fill="green")
+
     window.mainloop()
 
-def get_current_rms(device_name):
+def rms_generator(device_name):
     stream = sd.InputStream(device=device_name, channels=1, samplerate=44100)
     stream.start()
 
     try:
-        audio_data, _ = stream.read(1024)
-        rms_value = np.sqrt(np.mean(audio_data**2))
-        if rms_value > threshold:
-            if not muted:
-                print(f"Too loud! Muting {rms_value:.2f}")
-                change_mute()
-        else:
-            if muted:
-                print(f"Back to normal. Unmuting {rms_value:.2f}")
-                change_mute()
-        return rms_value
+        while True:
+            audio_data, _ = stream.read(1024)
+            rms_value = np.sqrt(np.mean(audio_data**2))
+            print(f"RMS value: {rms_value:.2f}")
+            yield rms_value
     except KeyboardInterrupt:
         print("\nMonitoring stopped by user.")
     finally:
